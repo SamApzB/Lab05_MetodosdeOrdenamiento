e0cab312392c2e28bcc276fa633ec8363ebaa72a
SamApzB <samantha.apaza@ucsm.edu.pe>
2023-04-19 01:00:47 -0500

    Ordenamiento

---
+++ Actividad02/TestSort.java
@@ -0,0 +1,11 @@
+package Actividad02;
+
+public class TestSort {
+
+	public static void showArray(int arr[]) {
+		for (int element : arr) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+}
